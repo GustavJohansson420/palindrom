8b4233fdcc70e148e1817caba8bb17f71d5dca2f
GustavJohansson420 <89460949+GustavJohansson420@users.noreply.github.com>
2021-11-12 16:02:48 +0100

    Add files via upload

---
+++ palindrom/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace palindrom
+{
+    class Program
+    {
+        static void Main(string[] args)
+         {  
+          int n,r,sum=0,temp;    
+          Console.Write("Skriv ett datum som t.ex dagens datum 211112: ");   
+          n = int.Parse(Console.ReadLine());  
+          temp=n;      
+          while(n>0)      
+          {      
+           r=n%10;      
+           sum=(sum*10)+r;      
+           n=n/10;      
+          }      
+          if(temp==sum)      
+           Console.Write("Ditt datum är en palindrom.");      
+          else      
+           Console.Write("Ditt datum är inte en palindrom.");     
+    }  
+  }  
+}
